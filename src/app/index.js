d178cbaf87757e597d17253abbf2ad5b99b91557
Yutaro HIGASHI <yutaro.higashi@notteco.jp>
2018-01-07 13:42:08 +0900

    added graph module and ajax function on static web

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,20 @@
+//var graph = require('fbgraph');
+
 exports.handler = (event, context, callback) => {
-    callback(null, {
-      "statusCode": "200",
-      "body": "JSON string"
-    });
+    /* if(event.access_token != null){
+      graph.setAccessToken(event.access_token);
+    }
+
+    graph.get("me", function(err, res) {
+      console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
+      callback(null, {
+        "statusCode": "200",
+        "body": res
+      });
+    }); */
+
+  callback(null, {
+    "statusCode": "200",
+    "body": '{result: "Hello, World!"}'
+  });
 };
